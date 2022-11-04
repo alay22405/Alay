268ab6cffc906b375759b422eba1cbf401fc886b
Mark Andrey Rubio <marrubio07@gmail.com>
2022-11-03 19:18:31 -0700

    added javadoc comments

--- Code/MainWindow.java
+++ Code/MainWindow.java
@@ -9,25 +9,43 @@ import java.util.ArrayList;
 public class MainWindow extends JFrame {
     private static final int DEFAULT_WIDTH = 600;
     private static final int DEFAULT_HEIGHT = 600;
-    public static String firstName;
-    public static String email;
+    private static String firstName;
+    private static String email;
     private ArrayList<String> developers = new ArrayList<String>();
-    public static JPanel mainPanel = new JPanel();
+    private static JPanel mainPanel = new JPanel();
 
+    /**
+     * Creates the main window, and add the main panel to the frame.
+     * It also adds all the names of the developers into an ArrayList
+     * @Author Mark Andrey Rubio
+     **/
     MainWindow(){
         //Adding the Developers of the app
         developers.add("Mark Andrey Rubio - this app needs more work.");
         developers.add("Salahuddin Majed");
         developers.add("Alay Kidane");
         developers.add("Arshdeep Singh");
+
+        //adding the main panel to the frame
         getContentPane().add(mainPanel);
+
+        //adding the main menu components like about and profile
         generateMenuPanel();
+
+        //setting the size and visibility of the main window
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         setVisible(true);
     }
 
+    /***
+     * Adds all the buttons of the main menu to the main panel and their events.
+     * @Author Mark Andrey Rubio
+     **/
     public void generateMenuPanel(){
+        //remove any pre-existing GUI components
         mainPanel.removeAll();
+
+        //creates the About and Profile Buttons
         var AboutButton = new JButton("About");
         AboutButton.setPreferredSize(new Dimension(100,60));
         AboutButton.setVisible(true);
@@ -38,6 +56,7 @@ public class MainWindow extends JFrame {
         mainPanel.add(AboutButton);
         mainPanel.add(SetProfileButton);
 
+        //About Button event
         AboutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -45,17 +64,25 @@ public class MainWindow extends JFrame {
             }
         });
 
+        //Profile Button event
         SetProfileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 SetProfilePanel();
             }
         });
+
+        //refresh the GUI/Window
         revalidate();
         repaint();
     }
 
+    /**
+     * Resets the main panel and adds the set profile gui components
+     * @Author Mark Andrey Rubio
+     * **/
     public void SetProfilePanel(){
+        //remove any pre-existing GUI components
         mainPanel.removeAll();
         var firstNameDisplay = new TextField(20);
         firstNameDisplay.setPreferredSize(new Dimension(20,50));
@@ -74,14 +101,21 @@ public class MainWindow extends JFrame {
         });
         emailButton.addActionListener(e -> email = emailDisplay.getText());
         backButton.addActionListener(e -> generateMenuPanel());
+
+        //refresh the GUI/Window
         revalidate();
         repaint();
     }
 
+    /**
+     * Resets the main panel and adds about panel components
+     * @Author Mark Andrey Rubio
+     * **/
     public void AboutPanel(){
+        //remove any pre-existing GUI components
         mainPanel.removeAll();
         var text = new JTextArea();
-        if (firstName == null || email == null) {
+        if (firstName == null) {
             text.setText("This is registered to no one\n");
             addDevelopers(text, developers);
         } else {
@@ -92,10 +126,15 @@ public class MainWindow extends JFrame {
         mainPanel.add(text);
         mainPanel.add(backButton);
         backButton.addActionListener(e -> generateMenuPanel());
+
+        //refresh the GUI/Window
         revalidate();
         repaint();
     }
 
+    /**
+     * A helper method to append the names of the developers to a JTextArea
+     * **/
     private void addDevelopers(JTextArea textArea, ArrayList<String> developers){
         textArea.append("This app is provided by: \n");
         for (String text: developers
