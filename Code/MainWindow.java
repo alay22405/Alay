8a9135f42b49feced02b9a22d40d6de1e53c024b
Mark Andrey Rubio <marrubio07@gmail.com>
2022-11-03 18:54:15 -0700

    alfjsadl;fjads;flkajsd;flakjdf;aljf;aldskjfa;oeijf

--- Code/MainWindow.java
+++ Code/MainWindow.java
@@ -85,7 +85,7 @@ public class MainWindow extends JFrame {
             text.setText("This is registered to no one\n");
             addDevelopers(text, developers);
         } else {
-            text.setText("This app is registered to: " + firstName);
+            text.setText("This app is registered to: " + firstName+ "\n");
             addDevelopers(text, developers);
         }
         var backButton = new JButton("Back");
@@ -102,52 +102,5 @@ public class MainWindow extends JFrame {
         ) {
             textArea.append(text + "\n");
         }
-    }
-}
-
-class AboutPanel extends JPanel {
-    AboutPanel(String UserFirstName, String UserEmail, ArrayList<String> developers){
-        var text = new JTextArea();
-        if (UserFirstName == null || UserEmail == null) {
-            text.setText("This is registered to no one\n");
-            addDevelopers(text, developers);
-        } else {
-            text.setText("This app is registered to: " + UserFirstName);
-            addDevelopers(text, developers);
-        }
-        add(text);
-    }
-
-    private void addDevelopers(JTextArea textArea, ArrayList<String> developers){
-        textArea.append("This app is provided by: \n");
-        for (String text: developers
-        ) {
-            textArea.append(text + "\n");
-        }
-    }
-
-}
-
-class SetProfilePanel extends JPanel
-{
-    //Default constructor
-    SetProfilePanel(String Name, String usermail) {
-        setSize(new Dimension(600, 600));
-        //Set Profile Panel Components
-        //setLayout(new GridLayout(2, 2));
-        var firstNameDisplay = new TextField(20);
-        firstNameDisplay.setPreferredSize(new Dimension(20,50));
-        var emailDisplay = new TextField(50);
-        emailDisplay.setPreferredSize(new Dimension(20,50));
-        var firstNameButton = new JButton("Set First Name");
-        var emailButton = new JButton("Set Email");
-        add(firstNameDisplay);
-        add(firstNameButton);
-        add(emailDisplay);
-        add(emailButton);
-
-        firstNameButton.addActionListener(e -> { MainWindow.firstName = firstNameDisplay.getText();
-        });
-        emailButton.addActionListener(e -> MainWindow.email = emailDisplay.getText());
     }
 }
